a9d57ce59c1937a7faccc50fd9e32a9f26ee9819
Remko Noteboom <remko@southpawtech.com>
2014-01-25 23:13:17 -0500

    DOCS: adjusted generate script to handle it being run from different folders and made it autocreate the build folder

--- bin/generate_books.py
+++ bin/generate_books.py
@@ -2,6 +2,20 @@
 
 import os, shutil
 
+import inspect
+path = inspect.getfile( inspect.currentframe() )
+path = path.replace("\\", "/")
+if path.startswith("/"):
+    base_dir = os.path.dirname(path)
+    base_dir = os.path.dirname(base_dir)
+else:
+    base_dir = ".."
+
+
+build_dir = "%s/build" % base_dir
+if not os.path.exists(build_dir):
+    os.makedirs(build_dir)
+
 
 books = [
     'tactic-developer',
@@ -10,32 +24,34 @@ books = [
 ]
 
 
+
+
 for book in books:
-    cmd = 'asciidoc -b docbook ../book/%s.adoc' % book
+    cmd = 'asciidoc -b docbook %s/book/%s.adoc' % (base_dir, book)
     print
     print cmd
     print
     os.system(cmd)
 
-    xml_path = "../build/%s.xml" % book
+    xml_path = "%s/build/%s.xml" % (base_dir, book)
     if os.path.exists(xml_path):
         os.unlink(xml_path)
-    shutil.move("../book/%s.xml" % book, xml_path)
+    shutil.move("%s/book/%s.xml" % (base_dir, book), xml_path)
 
     #cmd = 'xsltproc --nonet /etc/asciidoc/docbook-xsl/chunked.xsl %s.xml' % book
-    cmd = 'a2x -D ../build -f chunked %s' % xml_path
+    cmd = 'a2x -D %s/build -f chunked %s' % (base_dir, xml_path)
     print
     print cmd
     print
     os.system(cmd)
 
-    cmd = 'a2x -D ../build -f xhtml %s' % xml_path
+    cmd = 'a2x -D %s/build -f xhtml %s' % (base_dir, xml_path)
     print
     print cmd
     print
     os.system(cmd)
 
-    cmd = 'a2x -D ../build -f pdf %s' % xml_path
+    cmd = 'a2x -D %s/build -f pdf %s' % (base_dir, xml_path)
     print
     print cmd
     print
