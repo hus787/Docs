96f9357072b435c6fd2c19725c376dc1d8dc45f6
Remko Noteboom <remko@southpawtech.com>
2014-02-01 21:08:39 -0500

    DOCS: added some code to look for images

--- bin/generate_books.py
+++ bin/generate_books.py
@@ -3,11 +3,17 @@
 import os, shutil
 
 import inspect
+
+
+
+
 path = inspect.getfile( inspect.currentframe() )
 path = path.replace("\\", "/")
 if path.startswith("/"):
     base_dir = os.path.dirname(path)
     base_dir = os.path.dirname(base_dir)
+elif path.startswith("bin"):
+    base_dir = "."
 else:
     base_dir = ".."
 
@@ -15,6 +21,23 @@ else:
 build_dir = "%s/build" % base_dir
 if not os.path.exists(build_dir):
     os.makedirs(build_dir)
+
+
+def find_media(base_dir):
+
+    paths = []
+    for root, dirnames, basenames in os.walk(base_dir):
+
+        for dirname in dirnames:
+            if dirname == "media":
+                media_dir = "%s/%s" % (root, dirname)
+                media_basenames = os.listdir(media_dir)
+                for media_basename in media_basenames:
+                    path = "%s/%s" % (media_dir, media_basename)
+                    paths.append(path)
+    return paths
+
+
 
 
 books = [
@@ -27,11 +50,18 @@ books = [
 
 
 for book in books:
+
+    print "Looking for media: "
+    media_dir = "%s/section/doc/%s" % (base_dir, book)
+    paths = find_media(media_dir)
+    print " ... found %s images" % len(paths)
+
     cmd = 'asciidoc -b docbook %s/book/%s.adoc' % (base_dir, book)
     print
     print cmd
     print
     os.system(cmd)
+
 
     xml_path = "%s/build/%s.xml" % (base_dir, book)
     if os.path.exists(xml_path):
